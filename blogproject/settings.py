db8d77dde16b77da9b3f72a8aa1273c636e699f7
zhq1943 <engineer1943@163.com>
2018-03-26 23:15:02 +0800

    add templates

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '24c$yc62(i01jbp&@zfy2%us@6+i8ahqqa^#-=yo8_8c)q0#lt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','demo.zhaohqweb.com']
 
 
 # Application definition
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'DIRS':[os.path.join(BASE_DIR,'templates')],
             ],
         },
     },
